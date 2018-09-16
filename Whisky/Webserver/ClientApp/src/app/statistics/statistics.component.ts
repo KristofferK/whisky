1ec9a18636c93564b8bbebf744e7c62493edbc39
Kristoffer <fobber1995@gmail.com>
2018-09-16 20:05:51 +0200

    Add forced detection change after adding measurement when in production

--- Whisky/Webserver/ClientApp/src/app/statistics/statistics.component.ts
+++ Whisky/Webserver/ClientApp/src/app/statistics/statistics.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { Measurement } from '../../models/measurement';
 import { MeasurementService } from '../measurement.service';
 import { Subscription } from 'rxjs';
+import { environment } from './../../environments/environment';
 
 @Component({
   selector: 'app-statistics-component',
@@ -11,14 +12,16 @@ export class StatisticsComponent implements OnInit, OnDestroy {
   public measurements: Measurement[] = [];
   private measurementSubscription: Subscription;
 
-  constructor(private measurementService: MeasurementService) {
+  constructor(private measurementService: MeasurementService, private changeDetectorRef: ChangeDetectorRef) {
   }
 
   ngOnInit(): void {
     this.measurementSubscription = this.measurementService.measurements.subscribe(measurement => {
-      this.measurements = this.measurements.slice(0, 14);
+      this.measurements = this.measurements.slice(0, 49);
       this.measurements.unshift(measurement);
-      console.log('Added', measurement);
+      if (environment.production) {
+        this.changeDetectorRef.detectChanges();
+      }
     })
   }
 
