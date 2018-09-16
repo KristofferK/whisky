f31485b41a20019e33995a4f4c64f98c78d1e27e
Kristoffer <fobber1995@gmail.com>
2018-09-17 00:33:17 +0200

    Add interface for measurement persistence

---
+++ Whisky/Webserver/Persistence/Measurement/IMeasurementPersistence.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webserver.Persistence.Measurement
+{
+    public interface IMeasurementPersistence
+    {
+        void Insert(Models.Measurement measurement);
+        IEnumerable<Models.Measurement> GetLatest(int limit);
+        IEnumerable<Models.Measurement> GetLatest(int sensorID, int limit);
+    }
+}
