e79cbcc7291767a226d5e534a10a376de094cea1
Kristoffer <fobber1995@gmail.com>
2018-09-16 18:26:51 +0200

    Add dummy data to GetExistingMeasurements

--- Whisky/Webserver/Controllers/MeasurementController.cs
+++ Whisky/Webserver/Controllers/MeasurementController.cs
@@ -36,7 +36,74 @@ namespace Webserver.Controllers
         [HttpGet("[action]")]
         public IEnumerable<MeasurementCelsius> GetExistingMeasurements()
         {
-            return new List<MeasurementCelsius>();
+            return new List<MeasurementCelsius>()
+            {
+                new MeasurementCelsius()
+                {
+                    DateMeasured = new DateTime(2018, 9, 6, 18, 0, 0),
+                    Pressure = 1200,
+                    SensorID = "1",
+                    Temperature = 40.2
+                },
+                new MeasurementCelsius()
+                {
+                    DateMeasured = new DateTime(2018, 9, 6, 18, 0, 0),
+                    Pressure = 1202,
+                    SensorID = "2",
+                    Temperature = 40.3
+                },
+                new MeasurementCelsius()
+                {
+                    DateMeasured = new DateTime(2018, 9, 6, 18, 0, 0),
+                    Pressure = 1201,
+                    SensorID = "3",
+                    Temperature = 40.1
+                },
+
+                new MeasurementCelsius()
+                {
+                    DateMeasured = new DateTime(2018, 9, 6, 18, 5, 0),
+                    Pressure = 1201,
+                    SensorID = "1",
+                    Temperature = 44.9
+                },
+                new MeasurementCelsius()
+                {
+                    DateMeasured = new DateTime(2018, 9, 6, 18, 5, 0),
+                    Pressure = 1202,
+                    SensorID = "2",
+                    Temperature = 45
+                },
+                new MeasurementCelsius()
+                {
+                    DateMeasured = new DateTime(2018, 9, 6, 18, 5, 0),
+                    Pressure = 1200,
+                    SensorID = "3",
+                    Temperature = 45.1
+                },
+
+                new MeasurementCelsius()
+                {
+                    DateMeasured = new DateTime(2018, 9, 6, 18, 10, 0),
+                    Pressure = 1210,
+                    SensorID = "1",
+                    Temperature = 49.1
+                },
+                new MeasurementCelsius()
+                {
+                    DateMeasured = new DateTime(2018, 9, 6, 18, 10, 0),
+                    Pressure = 1210,
+                    SensorID = "2",
+                    Temperature = 50.2
+                },
+                new MeasurementCelsius()
+                {
+                    DateMeasured = new DateTime(2018, 9, 6, 18, 10, 0),
+                    Pressure = 1209,
+                    SensorID = "3",
+                    Temperature = 50
+                },
+            };
         }
     }
 }
