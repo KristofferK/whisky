9627af2184e5c11c23820ba9c4990f2237e7f1e0
Kristoffer <fobber1995@gmail.com>
2018-09-17 13:48:25 +0200

    Add a Mongo database class

---
+++ Whisky/Webserver/Persistence/Measurement/ConnectionStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webserver.Persistence.Measurement
+{
+    public class ConnectionStrings
+    {
+        public const string MONGO_CONNECTION_STRING = "mongodb://localhost:27017/<DATABASE>";
+    }
+}
---
+++ Whisky/Webserver/Persistence/Measurement/Mongo.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webserver.Models;
+
+namespace Webserver.Persistence.Measurement
+{
+    public class Mongo : IMeasurementPersistence
+    {
+        private MongoClient client;
+        public Mongo(string connectionString)
+        {
+            client = new MongoClient(connectionString);
+        }
+
+        public IEnumerable<Models.Measurement> GetLatest(int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Models.Measurement> GetLatest(int sensorID, int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Models.Measurement> GetLatest(DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Models.Measurement measurement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
